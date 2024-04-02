dbb1c968dbf34242926ed06a3ffed8c79a59265d
Spencerreid559 <70726571+Spencerreids@users.noreply.github.com>
2024-04-01 21:41:11 -0600

    Updated Quality Checker
    Also Added a Cleaner Version of Performance

--- QualityChecker.py
+++ QualityChecker.py
@@ -5,60 +5,143 @@ def myreader(filename:str)->list:
         reader = csv.reader(f)
         your_list = list(reader)
         return your_list
+    
+
+def csvPrinter(fileName:str, foundAlerts):
+    with open("./reports/"+fileName, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["Row" , "Value In Database ('|' is the seperator for the sub array)" , "Error Type"])
+
+        for i in foundAlerts:
+            writer.writerow(toReaderPrinter(i))
+    pass
+
+def toReaderPrinter(i:list):
+    formattedString = "["
+    for c in i[1]:
+        formattedString = formattedString + str(c) + " | "
+    formattedString = formattedString + "]"
+    return [i[0], formattedString, i[2]]
 
 def departmentReport():
     departments = myreader('Department_Information.csv')
-    departmentNames = []
-    departmentid = []
+    departmentNames = dict()
+    departmentid = dict()
     index = 1
+    foundAlerts = []
     for i in departments:
         dateString = i[2].split("/")
-        
-        if departmentNames.count(i[1]) > 0:
-            print(index, i, "Dupe Department Name")
-        if departmentid.count(i[0]) > 0:
-            print(index, i, "Dupe Department ID")
-        if  not (i[0] and i[1] and i[2]):
-            print(index, i, "Missing Data")
-        elif i[2] != "DOE":
-            if int(dateString[2])<1900:
-                print(index, i, "Date too early")
-        departmentNames.append(i[1])
-        departmentid.append(i[0])
+        if index != 1:
+            if  not (i[0] and i[1] and i[2]):
+                foundAlerts.append([index, i, "Missing Data"])
+            else:
+                result = departmentNames.get(i[1])
+                if result != None:
+                    foundAlerts.append([index,i,"Duplicate Department Name at index " + str(result)])
+                result = departmentid.get(i[0])
+                if  result != None:
+                    foundAlerts.append([index, i, "Duplicate Department ID at index " + str(result)])
+                if int(dateString[2])<=1900:
+                    foundAlerts.append([index, i, "Date too early"])
+                if (int(dateString[1]) < 0 or int(dateString[1]) > 31 or int(dateString[0]) < 0 or int(dateString[0]) >12):
+                    foundAlerts.append([index, i, "Invalid Date"])
+        departmentNames.update({i[1]:index})
+        departmentid.update({i[0]:index})
         index+=1
+    csvPrinter("department.csv", foundAlerts)
 
 def studentCouncelingReport():
     students = myreader('Student_Counceling_Information.csv')
     departments = myreader('Department_Information.csv')
     validDepID = []
+    foundAlerts = []
+    index = 1
+    seenStudentIDs = dict()
     for i in departments:
         validDepID.append(i[0])
-    index = 1
     for i in students:
-        if validDepID.count(i[4]) == 0:
-            print(index, i, "Invalid Admitted Department")
-        if not (i[0] and i[1] and i[2] and i[3] and i[4]):
-            print(index, i, "Missing Values")
+        if index != 1:
+            if not (i[0] and i[1] and i[2] and i[3] and i[4]):
+                foundAlerts.append([index, i, "Missing Values"])
+            else:
+                if validDepID.count(i[4]) == 0:
+                    foundAlerts.append([index, i, "Invalid Admitted Department"])
+                dateString = i[1].split("/")
+                if (int(dateString[1]) < 0 or int(dateString[1]) > 31 or int(dateString[0]) < 0 or int(dateString[0]) >12):
+                    foundAlerts.append([index, i, "Invalid Date"])
+                dateString = i[2].split("/")
+                if (int(dateString[1]) < 0 or int(dateString[1]) > 31 or int(dateString[0]) < 0 or int(dateString[0]) >12):
+                    foundAlerts.append([index, i, "Invalid Date"])
+                result = seenStudentIDs.get(i[0] + i[3])
+                if (result != None):
+                    foundAlerts.append([index, i, "Duplicate Student Entry at index " + str(result)])
+        seenStudentIDs.update({i[0] + i[3]:index})
         index+=1
+        
+    csvPrinter("studentCounceling.csv", foundAlerts)
 
 def studentPerformanceReport():
     performance = myreader('Student_Performance_Data.csv')
-    index=1
+    students = myreader('Student_Counceling_Information.csv')
+    validStudentID = dict()
     seenReports = dict()
+    foundAlerts = []
+    for i in students:
+        validStudentID.update({i[0]:"a"})
+    index=1
+    
     for i in performance:
         if index != 1:
             if not (i[0] and i[1] and i[2] and i[3] and i[4] and i[5]):
-                print(index, i, "Missing Values")
+                foundAlerts.append([index, i, "Missing Values"])
             else:
                 if int(i[4]) < 0 or int(i[4])>100:
-                    print(index, i, "Invalid Performance")
+                    foundAlerts.append([index, i, "Invalid Performance"])
                 if int(i[5]) < 0:
-                    print(index, i, "Invalid Effort Hours")
-                if seenReports.get(i[0] + i[2]) != None:
-                    print(index, i, "Duplicate Paper for student")
-        seenReports.update({i[0] + i[2]: "a" })
+                    foundAlerts.append([index, i, "Invalid Effort Hours"])
+                result = seenReports.get(i[0] + i[2])
+                if result != None:
+                    foundAlerts.append([index, i, "Duplicate Paper for Student at index " + str(result)])
+                if validStudentID.get(i[0]) != "a":
+                    foundAlerts.append([index, i, "Student not in System"])
+        seenReports.update({i[0] + i[2]: index })
         index+=1
+    csvPrinter("studentPreformance.csv", foundAlerts)
+    
 
-# studentPerformanceReport()
-# studentCouncelingReport()
+def employeeReport():
+    employees = myreader('Employee_Information.csv')
+    departments = myreader('Department_Information.csv')
+    validDepID = dict()
+    seenEmployeeID = dict()
+    foundAlerts = []
+    index = 1
+    for i in departments:
+        validDepID.update({i[0]:"a"})
+    for i in employees:
+        if index !=1:
+            if not (i[0] and i[1] and i[2] and i[3]):
+                foundAlerts.append([index, i, "Missing Values"])
+                return
+            else:
+                dateString = i[1].split("/")
+                if (int(dateString[1]) < 0 or int(dateString[1]) > 31 or int(dateString[0]) < 0 or int(dateString[0]) >12):
+                    foundAlerts.append([index, i, "Invalid Date"])
+                dateString = i[2].split("/")
+                if (int(dateString[1]) < 0 or int(dateString[1]) > 31 or int(dateString[0]) < 0 or int(dateString[0]) >12):
+                    foundAlerts.append([index, i, "Invalid Date"])
+                if validDepID.get(i[3]) != "a":
+                    foundAlerts.append([index, i, "Invalid Department"])
+                result = seenEmployeeID.get(i[0])
+                if result != None:
+                    foundAlerts.append([index, i, "Duplicate Employee ID at index " + str(result)])
+        seenEmployeeID.update({i[0]:index})
+        index+=1
+    csvPrinter("employee.csv", foundAlerts)
+
+    
+
+studentPerformanceReport()
+studentCouncelingReport()
 departmentReport()
+employeeReport()
